4e3c756d21f0a688db1b3298bbc1ba9a044cd738
Przemyslaw Sulecki <przemek.sulecki.mail@gmail.com>
2020-01-25 12:16:14 +0100

    Return doctor's appointments to his scheduler

--- src/main/java/pl/edu/utp/wtie/rejestracja/controller/DoctorController.java
+++ src/main/java/pl/edu/utp/wtie/rejestracja/controller/DoctorController.java
@@ -1,6 +1,10 @@
 package pl.edu.utp.wtie.rejestracja.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,14 +12,17 @@ import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import pl.edu.utp.wtie.rejestracja.model.Appointment;
 import pl.edu.utp.wtie.rejestracja.model.Doctor;
 import pl.edu.utp.wtie.rejestracja.model.Patient;
+import pl.edu.utp.wtie.rejestracja.repository.AppointmentRepository;
 import pl.edu.utp.wtie.rejestracja.repository.DoctorRepository;
 import pl.edu.utp.wtie.rejestracja.repository.PatientRepository;
 
@@ -33,9 +40,30 @@ public class DoctorController {
     @Autowired
     private PatientRepository patientRepository;
 
+    @Autowired
+    private AppointmentRepository appointmentRepository;
+
     @GetMapping("/doctor/signup")
     public String showSignUpForm(Doctor doctor) {
         return "doctor-sign-up";
+    }
+
+    @GetMapping("/panel/scheduler")
+    public String showDoctorScheduler(Appointment appointment, HttpSession session, Model model) {
+        if(session.getAttribute("doctor-logged") == null)
+            return "index";
+
+        String doctorEmail = (String)session.getAttribute("doctor-logged");
+        List<Appointment> doctorAppointments = new ArrayList<>();
+
+        Iterable<Appointment> appointments = appointmentRepository.findAll();
+        for(Appointment a : appointments)
+            if(doctorEmail.equals(a.getDoctor().getEmail()))
+                doctorAppointments.add(a);
+        
+        model.addAttribute("doctorAppointments", doctorAppointments);
+
+        return "doctor-scheduler";
     }
 
     @PostMapping("/doctor/add")
