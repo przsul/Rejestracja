f27af63b299eb5567f9c14a32026c57631af17b4
Przemyslaw Sulecki <przemek.sulecki.mail@gmail.com>
2020-01-28 23:02:32 +0100

    Modify AppointmentRepository

--- src/main/java/pl/edu/utp/wtie/rejestracja/repository/AppointmentRepository.java
+++ src/main/java/pl/edu/utp/wtie/rejestracja/repository/AppointmentRepository.java
@@ -1,12 +1,17 @@
 package pl.edu.utp.wtie.rejestracja.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import pl.edu.utp.wtie.rejestracja.model.Appointment;
+import pl.edu.utp.wtie.rejestracja.model.Doctor;
 
 /**
  * AppointmentRepository
  */
 @Repository
-public interface AppointmentRepository extends CrudRepository<Appointment, Long>{}
+public interface AppointmentRepository extends CrudRepository<Appointment, Long> {
+    List<Appointment> findByDoctorOrderByStartDateTime(Doctor doctor);
+}
